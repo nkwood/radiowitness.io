36fca0fd447f78f2d06b19c7f9d09795d04f99ba
felix.bley <felix.bley@w-ps-fbl>
2016-07-24 13:04:47 -0600

    Added webpack as build tool

--- bundle.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-browserify -t [ babelify --presets [ es2015 react ] ] js/main.js -o bundle.js
-lessc css/bundle.less bundle.css
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+var webpack           = require('webpack');
+var path              = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var BUILD_DIR   = path.resolve(__dirname, '');
+var APP_DIR     = path.resolve(__dirname, 'js');
+var STYLES_DIR  = path.resolve(__dirname, 'css');
+
+function getEntrySources(sources) {
+    if (process.env.NODE_ENV === 'dev') {
+        sources.push('webpack-dev-server/client?http://localhost:8080');
+        sources.push('webpack/hot/only-dev-server');
+    }
+    return sources;
+}
+
+var config = {
+  entry: getEntrySources([
+    'webpack-dev-server/client?http://localhost:8080',
+    APP_DIR    + '/main.js',
+    STYLES_DIR + '/bundle.less'
+  ]),
+  output: {
+    publicPath : 'http://localhost:8080/',
+    path       : BUILD_DIR,
+    filename   : 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test    : /\.js?/,
+        include : APP_DIR,
+        loaders : ['react-hot', 'babel']
+      },
+      {
+        test    : /\.less$/,
+        include : STYLES_DIR,
+        loader  : ExtractTextPlugin.extract('css!less') 
+      },
+      {
+        test    : /\.css$/,
+        include : STYLES_DIR,
+        loaders : ['style!css']
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("bundle.css")
+  ],
+  devtoo: 'eval'
+};
+
+module.exports = config;
